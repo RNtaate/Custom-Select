225dcd7c0bdf800fcceec1d1168c2d535ef98461
RNtaate <royhyde14@gmail.com>
2022-09-23 01:02:07 +0300

    Add toggling of showing the select list when clicking on the input
    Create state for toggling showing of the list

--- Custom Select/src/Select.tsx
+++ Custom Select/src/Select.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./select.module.css";
 
 type SelectOption = {
@@ -13,14 +13,24 @@ type SelectProps = {
 }
 
 export function Select({value, onChange, options}: SelectProps) {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div tabIndex={0} className={styles.container}>
-      <span className={styles.value}>Value</span>
-      <button className={styles["clear-btn"]}>&times;</button>
+    <div 
+      tabIndex={0}
+      onBlur = {() => setIsOpen(false)}
+      onClick = {() => setIsOpen(prev => !prev)}
+      className={styles.container}
+    >
+      <span className={styles.value}>{value?.label}</span>
+      <button 
+        className={styles["clear-btn"]}
+      >&times;</button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
 
-      <ul className={`${styles.options}`}>
+      <ul className={`${styles.options} ${isOpen? styles.show : ""}`}>
         {options.map( option => {
           return <li key={option.value} className={styles.option}>{option.label}</li>
         })}
